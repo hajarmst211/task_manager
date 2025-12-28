c223df11dc1065bb7e70d8abe0ae00d24fe85d3f
hajarmst211 <hajarmst21@gmail.com>
2025-12-28 15:19:07 +0100

    .

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -2,38 +2,38 @@
 
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE TYPE status_types AS ENUM ("not started", "in progress", "done")
-CREATE TYPE priorities AS ENUM ("high", "medium", "low")
-CREATE TYPES task_types AS ENUM ("to do list", "text", "date")
+CREATE TYPE status_types AS ENUM ('not started', 'in progress', 'done');
+CREATE TYPE priorities AS ENUM ('high', 'medium', 'low');
+CREATE TYPE task_types AS ENUM ('to do list', 'text', 'date');
 
 
 CREATE TABLE IF NOT EXISTS blocks(
-    block_id UUID UUID DEFAULT gen_random_uuid(),
+    block_id UUID DEFAULT gen_random_uuid(),
     parent_id UUID REFERENCES block(block_id) ON DELETE CASCADE,
     tasks TEXT NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS tasks(
-    task_id UUID DEFAULT gen_random_uuid(),
+    task_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     block_id UUID REFERENCES blocks(block_id),
     title TEXT NOT NULL,
     task_type task_types NOT NULL,
     is_done BOOLEAN DEFAULT FALSE,
-    status  status_types DEFAULT "not started",
+    status  status_types DEFAULT 'not started',
     properties JSONB DEFAULT '{}'::jsonb
 );
 
 
 CREATE TABLE IF NOT EXISTS tags(
-    tag_id UUID DEFAULT gen_random_uuid(),
+    tag_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     label TEXT NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS task_tags(
-    tag_id UUID REFERENCES tags(tag_id),
-    task_id UUID REFERENCES tasks(task_id),
+    tag_id UUID REFERENCES tags(tag_id) ON DELETE CASCADE,
+    task_id UUID REFERENCES tasks(task_id)ON DELETE CASCADE,
     PRIMARY KEY (task_id, tag_id)
 );
 
