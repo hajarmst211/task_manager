c62886e6bc440921c6fd9d9b30ab35ed5eaa3a37
hajarmst211 <hajarmst21@gmail.com>
2026-04-13 16:42:39 +0100

    remake

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"taskManager/model"
-	"time"
-	"fmt"
+//	"fmt"
+	"taskManager/helpers"
+//	"taskManager/model"
+//	"time"
 )
 func main() {
-	layout := "2006-01-02"
-	deadline,_ := time.Parse(layout, "2025-05-01")
+/*	deadline,_ := time.Parse(layout, "2025-05-01")
 	createdAT, _:= time.Parse(layout, "2025-05-01")
 	newTask, err := task.NewTask("finish planner", false, createdAT, deadline)
 	if err != nil{
 		fmt.Println(err)
 	}
-	fmt.Println(newTask)
+
+	database.Trydb()
+	task_err := SaveTask(newTask)
+
+	fmt.Println("this is the new task error:" ,task_err)
+	*/
+database.Trydb()	
 }
